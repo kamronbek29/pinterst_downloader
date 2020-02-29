c78f4969533272b22b014e051238243c7c9fa8c4
kamronbek <osimjonov2001@gmail.com>
2020-02-29 14:19:17 +0500

    Pinterest downloader

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pinterst_downloader
  Simple python script to download photo and video from pinterest
+
+''
---
+++ pinterest-downloader.py
@@ -0,0 +1,51 @@
+from urllib import request
+
+import requests
+from pyquery import PyQuery as pq
+
+
+# Function to get download url
+def get_download_url(link):
+    # Make request to website 
+    post_request = requests.post('https://www.expertsphp.com/download.php', data={'url': link})
+
+    # Get content from post request
+    request_content = post_request.content
+    str_request_content = str(request_content, 'utf-8')
+    download_url = pq(str_request_content)('table.table-condensed')('tbody')('td')('a').attr('href')
+    
+    if '.mp4' in str(download_url):
+        print('Pinterest videeo download url: ', download_url)
+    else:
+        print('Pinterest image download url: ', download_url)
+
+    return download_url
+
+
+# Function to download video
+def download_video(url):
+    video_to_download = request.urlopen(url).read()
+    with open('pinterest_video.mp4', 'wb') as video_stream:
+        video_stream.write(video_to_download)
+
+
+# Function to download image
+def download_image(url):
+    image_to_download = request.urlopen(url).read()
+    with open('pinterest_iamge.jpg', 'wb') as photo_stream:
+        photo_stream.write(image_to_download)
+
+
+if __name__ == "__main__":
+    url = input('Please, put your pinterest url: ')
+    what_to_download = input('\nWhat do you want to download?\n1. Video\n2. Photo\nWrite here 1 or 2: ')
+
+    print('\nPlease, wait, your file is downloading!')
+
+    download_url = get_download_url('https://www.pinterest.com/pin/734227545487221777/')
+    if str(what_to_download) == '1':
+        download_video(download_url)
+    else:
+        download_image(download_url)
+
+    print('Your file was successfully saved!')
